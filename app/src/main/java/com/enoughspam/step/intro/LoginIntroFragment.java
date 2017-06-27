74feb21b5eb0f0f239e8a09fca117915fc3d3ace
Hugo <HugoCastelaniBP@gmail.com>
2017-06-27 17:00:25 -0300

    Google-services.json generated and placed; Implemented Google's sign in button in login slide; Implemented simplest actions to Google's sign in button in Login Intro Fragment

--- app/src/main/java/com/enoughspam/step/intro/LoginIntroFragment.java
+++ app/src/main/java/com/enoughspam/step/intro/LoginIntroFragment.java
@@ -1,24 +1,80 @@
 package com.enoughspam.step.intro;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.enoughspam.step.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class LoginIntroFragment extends Fragment {
+public class LoginIntroFragment extends Fragment implements
+        GoogleApiClient.OnConnectionFailedListener {
 
+    private static final int RC_SIGN_IN = -3;
+
+    private SignInButton googleButton;
+    private GoogleApiClient googleApiClient;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.intro_fragment_login, container, false);
 
+        googleButton = (SignInButton) view.findViewById(R.id.intro_google_button);
+        googleButton.setColorScheme(SignInButton.COLOR_DARK);
+        googleButton.setSize(SignInButton.SIZE_STANDARD);
 
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onGoogleButtonClick();
+            }
+        });
+
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestId()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(getActivity())
+                .enableAutoManage(getActivity(), this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions)
+                .build();
 
         return view;
     }
 
+    private void onGoogleButtonClick() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount acct = result.getSignInAccount();
+
+                /*mFullName = acct.getDisplayName()
+                mEmail = acct.getEmail();*/
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
