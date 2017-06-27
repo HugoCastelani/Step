99aa8eb5d0f0ec91c6352b46bcaa775529745428
Hugo <HugoCastelaniBP@gmail.com>
2017-06-27 15:09:49 -0300

    Removed unnecessary autoincrement from integer primary keys; Removed "\n" that came from Step Admin database sync

--- app/src/main/java/com/enoughspam/step/database/dao/DatabaseHelper.java
+++ app/src/main/java/com/enoughspam/step/database/dao/DatabaseHelper.java
@@ -23,134 +23,133 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         // RELATED TABLES
 
         // create user table
-        sqLiteDatabase.execSQL("create table user (\n" +
-                "id integer primary key not null auto_increment,\n" +
+        sqLiteDatabase.execSQL("create table user (" +
+                "id integer primary key not null," +
                 "name varchar(50) not null);");
 
         // create friendship table
-        sqLiteDatabase.execSQL("create table friendship (\n" +
-                "id integer primary key not null auto_increment,\n" +
-                "idUserAdded integer not null,\n" +
-                "idUserAdding integer not null,\n" +
-                "foreign key(idUserAdded) references user(id),\n" +
+        sqLiteDatabase.execSQL("create table friendship (" +
+                "id integer primary key not null," +
+                "idUserAdded integer not null," +
+                "idUserAdding integer not null," +
+                "foreign key(idUserAdded) references user(id)," +
                 "foreign key(idUserAdding) references user(id));");
 
         // creating country table
-        sqLiteDatabase.execSQL("create table country (\n" +
-                "id integer primary key not null auto_increment,\n" +
+        sqLiteDatabase.execSQL("create table country (" +
+                "id integer primary key not null," +
                 "name varchar(50) not null)");
 
         // creating country code table
-        sqLiteDatabase.execSQL("create table country_code (\n" +
-                "code varchar(10) primary key not null,\n" +
-                "idCountry integer not null,\n" +
+        sqLiteDatabase.execSQL("create table country_code (" +
+                "code varchar(10) primary key not null," +
+                "idCountry integer not null," +
                 "foreign key(idCountry) references country(id));");
 
         // creating state table
-        sqLiteDatabase.execSQL("create table state (\n" +
-                "id integer primary key not null auto_increment,\n" +
-                "name varchar(50) not null,\n" +
-                "idCountry integer not null,\n" +
+        sqLiteDatabase.execSQL("create table state (" +
+                "id integer primary key not null," +
+                "name varchar(50) not null," +
+                "idCountry integer not null," +
                 "foreign key(idCountry) references country(id));");
 
         // creating area code table
-        sqLiteDatabase.execSQL("create table area_code (\n" +
-                "code varchar(10) primary key not null,\n" +
-                "name varchar(50) not null,\n" +
-                "idState integer not null,\n" +
+        sqLiteDatabase.execSQL("create table area_code (" +
+                "code varchar(10) primary key not null," +
+                "name varchar(50) not null," +
+                "idState integer not null," +
                 "foreign key(idState) references state(id));");
 
         // creating phone table
-        sqLiteDatabase.execSQL("create table phone (\n" +
-                "id integer primary key not null auto_increment,\n" +
-                "number varchar(20) not null,\n" +
-                "areaCode varchar(10) not null,\n" +
-                "idUser integer not null,\n" +
-                "foreign key(areaCode) references area_code(code),\n" +
+        sqLiteDatabase.execSQL("create table phone (" +
+                "id integer primary key not null," +
+                "number varchar(20) not null," +
+                "areaCode varchar(10) not null," +
+                "idUser integer not null," +
+                "foreign key(areaCode) references area_code(code)," +
                 "foreign key(idUser) references user(id));");
 
         // creating notification table
-        sqLiteDatabase.execSQL("create table notification (\n" +
-                "id integer primary key not null auto_increment,\n" +
-                "idPhone integer not null,\n" +
-                "idUserNotified integer not null,\n" +
-                "foreign key(idPhone) references phone(id),\n" +
+        sqLiteDatabase.execSQL("create table notification (" +
+                "id integer primary key not null," +
+                "idPhone integer not null," +
+                "idUserNotified integer not null," +
+                "foreign key(idPhone) references phone(id)," +
                 "foreign key(idUserNotified) references user(id));");
 
         // creating denunciation table
-        sqLiteDatabase.execSQL("create table denunciation (\n" +
-                "id integer primary key not null auto_increment,\n" +
-                "idPhone integer not null,\n" +
+        sqLiteDatabase.execSQL("create table denunciation (" +
+                "id integer primary key not null," +
+                "idPhone integer not null," +
                 "foreign key(idPhone) references phone(id));");
 
         // creating description table
-        sqLiteDatabase.execSQL("create table description (\n" +
-                "id integer primary key not null auto_increment,\n" +
+        sqLiteDatabase.execSQL("create table description (" +
+                "id integer primary key not null," +
                 "description varchar(50) not null);");
 
         // creating denunciation/description table
-        sqLiteDatabase.execSQL("create table denunciation_description (\n" +
-                "idDenunciation integer not null,\n" +
-                "idDescription integer not null,\n" +
-                "primary key(idDenunciation, idDescription),\n" +
-                "foreign key(idDenunciation) references denunciation(id),\n" +
+        sqLiteDatabase.execSQL("create table denunciation_description (" +
+                "idDenunciation integer not null," +
+                "idDescription integer not null," +
+                "primary key(idDenunciation, idDescription)," +
+                "foreign key(idDenunciation) references denunciation(id)," +
                 "foreign key(idDescription) references description(id));");
 
         // creating suspicious treatment table
-        sqLiteDatabase.execSQL("create table suspicious_treatment (\n" +
-                "id integer primary key not null auto_increment,\n" +
+        sqLiteDatabase.execSQL("create table suspicious_treatment (" +
+                "id integer primary key not null," +
                 "name varchar(20) not null);");
 
         // creating treatment configuration table
-        sqLiteDatabase.execSQL("create table config_treatment (\n" +
-                "idDescription integer not null,\n" +
-                "idSuspiciousTreatment integer not null,\n" +
-                "primary key(idDescription, idSuspiciousTreatment),\n" +
-                "foreign key(idDescription) references description(id),\n" +
+        sqLiteDatabase.execSQL("create table config_treatment (" +
+                "idDescription integer not null," +
+                "idSuspiciousTreatment integer not null," +
+                "primary key(idDescription, idSuspiciousTreatment)," +
+                "foreign key(idDescription) references description(id)," +
                 "foreign key(idSuspiciousTreatment) references suspicious_treatment(id));");
 
         // NOT RELATED TABLES
 
         // creating feedback configuration table
-        sqLiteDatabase.execSQL("create table config_feedback (\n" +
-                "nmCallKind varchar(20) primary key  not null,\n" +
+        sqLiteDatabase.execSQL("create table config_feedback (" +
+                "nmCallKind varchar(20) primary key  not null," +
                 "showFeedback integer not null);");
 
         // creating service blocking configuration table
-        sqLiteDatabase.execSQL("create table config_service_block (\n" +
-                "nmService varchar(20) primary key not null,\n" +
+        sqLiteDatabase.execSQL("create table config_service_block (" +
+                "nmService varchar(20) primary key not null," +
                 "block integer not null);");
 
         // creating guide configuration table
-        sqLiteDatabase.execSQL("create table config_guide (\n" +
-                "nmView varchar(20) primary key not null,\n" +
+        sqLiteDatabase.execSQL("create table config_guide (" +
+                "nmView varchar(20) primary key not null," +
                 "showGuide integer not null);");
 
         // creating intro configuration table
-        sqLiteDatabase.execSQL("create table config_intro (\n" +
+        sqLiteDatabase.execSQL("create table config_intro (" +
                 "showIntro integer primary key not null);");
 
         sqLiteDatabase.execSQL("insert into config_intro values(1);");
 
         // creating network to download DB configuration table
-        sqLiteDatabase.execSQL("create table config_network_download_db (\n" +
-                "nmNetwork varchar(20) primary key not null,\n" +
+        sqLiteDatabase.execSQL("create table config_network_download_db (" +
+                "nmNetwork varchar(20) primary key not null," +
                 "downloadIt integer not null);");
 
         // creating suspicious denunciations amount configuration table
-        sqLiteDatabase.execSQL("create table suspicious_denunciations_amount (\n" +
+        sqLiteDatabase.execSQL("create table suspicious_denunciations_amount (" +
                 "amount integer primary key not null);");
 
         // creating theme configuration table
-        sqLiteDatabase.execSQL("create table config_theme (\n" +
-                "isDark integer primary key not null,\n" +
-                "accentColor char(7) not null,\n" +
+        sqLiteDatabase.execSQL("create table config_theme (" +
+                "isDark integer primary key not null," +
+                "accentColor char(7) not null," +
                 "defaultAccentColor char(7) not null);");
 
         sqLiteDatabase.execSQL("insert into config_theme values(0, '#00BCD4', '#00BCD4');");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-    }
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {}
 }
