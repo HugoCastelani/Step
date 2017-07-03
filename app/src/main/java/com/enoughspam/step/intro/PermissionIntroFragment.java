bf27e76148ce68e1a4cda8135488496f1a415050
Hugo <HugoCastelaniBP@gmail.com>
2017-07-03 16:38:57 -0300

    Permissions button get disabled when all permissions are granted

--- app/src/main/java/com/enoughspam/step/intro/PermissionIntroFragment.java
+++ app/src/main/java/com/enoughspam/step/intro/PermissionIntroFragment.java
@@ -2,14 +2,11 @@ package com.enoughspam.step.intro;
 
 
 import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 import com.afollestad.aesthetic.AestheticButton;
 import com.enoughspam.step.R;
 import com.heinrichreimersoftware.materialintro.app.SlideFragment;
@@ -48,37 +45,20 @@ public class PermissionIntroFragment extends SlideFragment {
                         Manifest.permission.READ_CONTACTS)
                 .subscribe(granted -> {
                     if (granted) {
+                        button.setEnabled(false);
+                        button.setText(getResources().getString(R.string.granted_permissions));
+
                         canGoForward = true;
                         canGoForward();
                         nextSlide();
+
                     } else {
+
                         Snackbar.make(getView(),
                                 getResources().getString(R.string.all_permissions_required),
                                 Snackbar.LENGTH_SHORT)
                                 .show();
                     }
                 });
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        if (requestCode == REQUEST_CODE) {
-            Toast.makeText(getActivity(), "requestCode = REQUEST_CODE", Toast.LENGTH_SHORT).show();
-            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_PHONE_STATE)
-                    == PackageManager.PERMISSION_GRANTED
-                    && ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_SMS)
-                    == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(getActivity(), "Both permissions granted", Toast.LENGTH_SHORT).show();
-
-                button.setActivated(false);
-                button.setText(getResources().getString(R.string.granted_permissions));
-
-                canGoForward = true;
-                canGoForward();
-                nextSlide();
-            }
-        } else {
-            Toast.makeText(getActivity(), "requestCode != REQUEST_CODE", Toast.LENGTH_SHORT).show();
-        }
     }
 }
