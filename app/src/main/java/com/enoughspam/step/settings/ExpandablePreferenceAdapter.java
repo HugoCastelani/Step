039c5c7404a2ee48523b7e812833248890d101bd
Hugo <HugoCastelaniBP@gmail.com>
2017-07-22 23:54:52 -0300

    Finish ExpandablePreference

--- app/src/main/java/com/enoughspam/step/settings/ExpandablePreferenceAdapter.java
+++ app/src/main/java/com/enoughspam/step/settings/ExpandablePreferenceAdapter.java
@@ -10,8 +10,10 @@ import android.view.ViewGroup;
 import com.afollestad.aesthetic.AestheticTextView;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.enoughspam.step.R;
+import com.enoughspam.step.annotation.NonNegative;
 import com.enoughspam.step.database.dao.DescriptionDAO;
 import com.enoughspam.step.database.dao.TreatmentDAO;
+import com.enoughspam.step.database.domains.Description;
 
 import java.util.List;
 
@@ -23,14 +25,15 @@ import java.util.List;
 
 public class ExpandablePreferenceAdapter extends RecyclerView.Adapter<ExpandablePreferenceAdapter.MyViewHolder> {
 
+    private final DescriptionDAO descriptionDAO;
     private final List<String> descriptionStringList;
     private final List<String> treatmentStringList;
 
     public ExpandablePreferenceAdapter(@NonNull final Context context) {
-        DescriptionDAO descriptionDAO = new DescriptionDAO(context);
+        descriptionDAO = new DescriptionDAO(context);
         this.descriptionStringList = descriptionDAO.getColumnList(descriptionDAO.DESCRIPTION);
 
-        TreatmentDAO treatmentDAO = new TreatmentDAO(context);
+        final TreatmentDAO treatmentDAO = new TreatmentDAO(context);
         this.treatmentStringList = treatmentDAO.getColumnList(treatmentDAO.TREATMENT);
     }
 
@@ -42,23 +45,38 @@ public class ExpandablePreferenceAdapter extends RecyclerView.Adapter<Expandable
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
         final String itemText = descriptionStringList.get(position);
 
         holder.description.setText(itemText);
         holder.description.setOnClickListener(v ->
+
             new MaterialDialog.Builder(holder.description.getContext())
                     .title(itemText)
                     .items(treatmentStringList)
                     .positiveText(R.string.done_button)
                     .negativeText(R.string.cancel_button)
-                    .itemsCallbackSingleChoice(0,
+                    .itemsCallbackSingleChoice(getSelectedTreatment(position),
                             ((dialog, itemView, which, text) -> true))
-                    .onPositive(((dialog, which) -> {
-                        dialog.getSelectedIndex();
-                    }))
+                    .onPositive(((dialog, which) ->
+                        setSelectedTreatment(position, dialog.getSelectedIndex())))
                     .show()
         );
+    }
+
+    /*
+     * ID isn't automatically generated in description and suspicious_treatment
+     * tables, so it doesn't start with 1, which allows me to use list's positions
+     */
+
+    private int getSelectedTreatment(@NonNegative int position) {
+        return (descriptionDAO.findById(position).getTreatmentId());
+    }
+
+    private void setSelectedTreatment(@NonNegative int position, @NonNegative int selected) {
+        final Description description = descriptionDAO.findById(position);
+        description.setTreatmentId(selected);
+        descriptionDAO.update(description);
     }
 
     @Override
