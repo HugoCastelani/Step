52b5d19cba74304cb164b6b52c4a2dfec0c0ffd7
Hugo <HugoCastelaniBP@gmail.com>
2017-07-30 23:17:03 -0300

    Move call list creation to fragment

--- app/src/main/java/com/enoughspam/step/addNumber/AddNumberAdapter.java
+++ app/src/main/java/com/enoughspam/step/addNumber/AddNumberAdapter.java
@@ -1,8 +1,5 @@
 package com.enoughspam.step.addNumber;
 
-import android.database.Cursor;
-import android.net.Uri;
-import android.provider.CallLog;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -17,17 +14,10 @@ import com.afollestad.sectionedrecyclerview.SectionedRecyclerViewAdapter;
 import com.afollestad.sectionedrecyclerview.SectionedViewHolder;
 import com.enoughspam.step.R;
 import com.enoughspam.step.database.dao.AreaDAO;
-import com.enoughspam.step.database.dao.CountryDAO;
-import com.enoughspam.step.database.dao.PersonalDAO;
-import com.enoughspam.step.database.dao.UserPhoneDAO;
-import com.enoughspam.step.database.domain.Area;
-import com.enoughspam.step.database.domain.Country;
 import com.enoughspam.step.database.domain.Phone;
-import com.enoughspam.step.database.domain.UserPhone;
 import com.enoughspam.step.domain.Call;
 import com.enoughspam.step.numberForm.NumberFormFragment;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -43,73 +33,9 @@ public class AddNumberAdapter extends SectionedRecyclerViewAdapter<SectionedView
     private final List<Call> mCallList;
     private final AddNumberFragment mFragment;
 
-    public AddNumberAdapter(@NonNull final AddNumberFragment fragment) {
-        Uri calls = Uri.parse("content://call_log/calls");
-        Cursor cursor = fragment.getContext().getContentResolver().query(calls, null, null, null, null);
-
-        mCallList = new ArrayList<>();
-        List<String> numberList = new ArrayList<>();
-
-        while (cursor.moveToNext()) {
-            String name = cursor.getString(cursor.getColumnIndex(CallLog.Calls.CACHED_NAME));
-            String number = cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER));
-            final String countryCode = cursor.getString(cursor.getColumnIndex(CallLog.Calls.COUNTRY_ISO));
-
-            // doesn't make sense show null numbers or outgoing calls
-            if (number == null ||
-                cursor.getInt(cursor.getColumnIndex(CallLog.Calls.TYPE)) == CallLog.Calls.OUTGOING_TYPE) {
-                continue;
-            }
-
-            // some names are unknown
-            if (name == null || name.equals(number)) {
-                name = fragment.getResources().getString(R.string.unknown);
-            }
-
-            // removing any 0 left
-            number = String.valueOf(Long.parseLong(number));
-
-            final Country country = CountryDAO.findByIso(countryCode);
-            Phone phone;
-
-            // some countries don't have area code
-            if (!country.getMask().isEmpty()) {
-
-                // discover number's area and country
-                StringBuilder areaCode = new StringBuilder(50);
-                Area area = null;
-                final int codeLength = String.valueOf(country.getCode()).length();
-
-                for (int i = 0; i < codeLength; i++) {
-                    areaCode.append(number.charAt(i));
-
-                    area = AreaDAO.findByCode(Integer.valueOf(areaCode.toString()));
-                    if (area != null && area.getState().getCountry().getId() == country.getId()) {
-                        number = number.substring(i + 1, number.length());
-                    }
-                }
-
-                if (area == null) continue;
-                else phone = new Phone(Long.parseLong(number), area);
-
-            } else {
-
-                phone = new Phone(Long.parseLong(number), country);
-            }
-
-            // see if number is already blocked
-            if (UserPhoneDAO.isAlreadyBlocked(new UserPhone(PersonalDAO.get(), phone, false))) {
-                continue;
-            }
-
-            final Call call = new Call(name, phone);
-
-            if (!numberList.contains(number)) {
-                mCallList.add(call);
-                numberList.add(number);
-            }
-        }
-
+    public AddNumberAdapter(@NonNull final List<Call> callList,
+                            @NonNull final AddNumberFragment fragment) {
+        mCallList = callList;
         mFragment = fragment;
     }
 
--- app/src/main/java/com/enoughspam/step/addNumber/AddNumberFragment.java
+++ app/src/main/java/com/enoughspam/step/addNumber/AddNumberFragment.java
@@ -1,6 +1,9 @@
 package com.enoughspam.step.addNumber;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CallLog;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,13 +16,21 @@ import com.afollestad.aesthetic.AestheticRecyclerView;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.blankj.utilcode.util.ConvertUtils;
 import com.enoughspam.step.R;
+import com.enoughspam.step.database.dao.AreaDAO;
+import com.enoughspam.step.database.dao.CountryDAO;
 import com.enoughspam.step.database.dao.PersonalDAO;
 import com.enoughspam.step.database.dao.UserPhoneDAO;
+import com.enoughspam.step.database.domain.Area;
+import com.enoughspam.step.database.domain.Country;
 import com.enoughspam.step.database.domain.Phone;
 import com.enoughspam.step.database.domain.UserPhone;
+import com.enoughspam.step.domain.Call;
 import com.enoughspam.step.util.EndOffsetItemDecoration;
 import com.enoughspam.step.util.ListDecorator;
 import com.enoughspam.step.util.ThemeHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Hugo Castelani
@@ -33,6 +44,10 @@ public class AddNumberFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private CustomLinearLayoutManager mLayoutManager;
 
+    public CustomLinearLayoutManager getLayoutManager() {
+        return mLayoutManager;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.add_number_fragment, container, false);
@@ -45,7 +60,7 @@ public class AddNumberFragment extends Fragment {
     private void initViews() {
         mRecyclerView = (AestheticRecyclerView) view.findViewById(R.id.add_number_recycler_view);
 
-        final AddNumberAdapter adapter = new AddNumberAdapter(this);
+        final AddNumberAdapter adapter = new AddNumberAdapter(getCallList(), this);
         mLayoutManager = new CustomLinearLayoutManager(mRecyclerView.getContext(),
                 LinearLayoutManager.VERTICAL, false);
 
@@ -57,8 +72,74 @@ public class AddNumberFragment extends Fragment {
         ListDecorator.addAdaptableMargins(mRecyclerView);
     }
 
-    public CustomLinearLayoutManager getLayoutManager() {
-        return mLayoutManager;
+    private List<Call> getCallList() {
+        Uri calls = Uri.parse("content://call_log/calls");
+        Cursor cursor = getContext().getContentResolver().query(calls, null, null, null, null);
+
+        List<Call> callList = new ArrayList<>();
+        List<String> numberList = new ArrayList<>();
+
+        while (cursor.moveToNext()) {
+            String name = cursor.getString(cursor.getColumnIndex(CallLog.Calls.CACHED_NAME));
+            String number = cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER));
+            final String countryCode = cursor.getString(cursor.getColumnIndex(CallLog.Calls.COUNTRY_ISO));
+
+            // doesn't make sense show null numbers or outgoing calls
+            if (number == null ||
+                    cursor.getInt(cursor.getColumnIndex(CallLog.Calls.TYPE)) == CallLog.Calls.OUTGOING_TYPE) {
+                continue;
+            }
+
+            // some names are unknown
+            if (name == null || name.equals(number)) {
+                name = getResources().getString(R.string.unknown);
+            }
+
+            // removing any 0 left
+            number = String.valueOf(Long.parseLong(number));
+
+            final Country country = CountryDAO.findByIso(countryCode);
+            Phone phone;
+
+            // some countries don't have area code
+            if (!country.getMask().isEmpty()) {
+
+                // discover number's area and country
+                StringBuilder areaCode = new StringBuilder(50);
+                Area area = null;
+                final int codeLength = String.valueOf(country.getCode()).length();
+
+                for (int i = 0; i < codeLength; i++) {
+                    areaCode.append(number.charAt(i));
+
+                    area = AreaDAO.findByCode(Integer.valueOf(areaCode.toString()));
+                    if (area != null && area.getState().getCountry().getId() == country.getId()) {
+                        number = number.substring(i + 1, number.length());
+                    }
+                }
+
+                if (area == null) continue;
+                else phone = new Phone(Long.parseLong(number), area);
+
+            } else {
+
+                phone = new Phone(Long.parseLong(number), country);
+            }
+
+            // see if number is already blocked
+            if (UserPhoneDAO.isAlreadyBlocked(new UserPhone(PersonalDAO.get(), phone, false))) {
+                continue;
+            }
+
+            final Call call = new Call(name, phone);
+
+            if (!numberList.contains(number)) {
+                callList.add(call);
+                numberList.add(number);
+            }
+        }
+
+        return callList;
     }
 
     public void confirmNumber(@NonNull final Phone phone) {
@@ -98,7 +179,7 @@ public class AddNumberFragment extends Fragment {
                 .show();
     }
 
-    public void saveNumber(@NonNull final Phone phone) {
+    protected void saveNumber(@NonNull final Phone phone) {
         UserPhoneDAO.create(new UserPhone(PersonalDAO.get(), phone, false));
         getActivity().onBackPressed();
     }
