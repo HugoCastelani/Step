aa2f298aa4d4552b79fbb2fb883693ccc6c7a95e
Hugo <HugoCastelaniBP@gmail.com>
2017-08-09 22:14:45 -0300

    Move AddNumberFragment's phone generation to phone class The method will be used in the class that extends BroadcastReceiver.

--- app/src/main/java/com/enoughspam/step/addNumber/AddNumberFragment.java
+++ app/src/main/java/com/enoughspam/step/addNumber/AddNumberFragment.java
@@ -17,13 +17,10 @@ import com.afollestad.materialdialogs.MaterialDialog;
 import com.blankj.utilcode.util.ConvertUtils;
 import com.enoughspam.step.R;
 import com.enoughspam.step.addNumber.extra.CustomLinearLayoutManager;
-import com.enoughspam.step.database.domain.Area;
-import com.enoughspam.step.database.domain.Country;
 import com.enoughspam.step.database.domain.Phone;
 import com.enoughspam.step.database.domain.UserPhone;
 import com.enoughspam.step.database.localDao.LUserDAO;
-import com.enoughspam.step.database.wideDao.AreaDAO;
-import com.enoughspam.step.database.wideDao.CountryDAO;
+import com.enoughspam.step.database.localDao.LUserPhoneDAO;
 import com.enoughspam.step.database.wideDao.UserPhoneDAO;
 import com.enoughspam.step.domain.Call;
 import com.enoughspam.step.util.EndOffsetItemDecoration;
@@ -88,11 +85,16 @@ public class AddNumberFragment extends Fragment {
         while (cursor.moveToNext()) {
             String name = cursor.getString(cursor.getColumnIndex(CallLog.Calls.CACHED_NAME));
             String number = cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER));
-            final String countryCode = cursor.getString(cursor.getColumnIndex(CallLog.Calls.COUNTRY_ISO));
+            final String iso = cursor.getString(cursor.getColumnIndex(CallLog.Calls.COUNTRY_ISO));
+
+            // check if number already is on the list
+            if (!numberList.contains(number)) {
+                numberList.add(number);
+            } else continue;
 
             // doesn't make sense show null numbers or outgoing calls
-            if (number == null ||
-                    cursor.getInt(cursor.getColumnIndex(CallLog.Calls.TYPE)) == CallLog.Calls.OUTGOING_TYPE) {
+            if (number == null || cursor.getInt(cursor.getColumnIndex
+                    (CallLog.Calls.TYPE)) == CallLog.Calls.OUTGOING_TYPE) {
                 continue;
             }
 
@@ -101,48 +103,15 @@ public class AddNumberFragment extends Fragment {
                 name = getResources().getString(R.string.unknown);
             }
 
-            // removing any 0 left
-            number = String.valueOf(Long.parseLong(number));
+            Phone phone = Phone.generateObject(number, iso);
+            if (phone == null) continue;    // number doesn't exist
 
-            final Country country = CountryDAO.findByIso(countryCode);
-            Phone phone;
-
-            // some countries don't have area code
-            if (!country.getMask().isEmpty()) {
-
-                // discover number's area and country
-                StringBuilder areaCode = new StringBuilder(50);
-                Area area = null;
-                final int codeLength = String.valueOf(country.getCode()).length();
-
-                for (int i = 0; i < codeLength; i++) {
-                    areaCode.append(number.charAt(i));
-
-                    area = AreaDAO.findByCode(Integer.valueOf(areaCode.toString()));
-                    if (area != null && area.getState().getCountry().getId() == country.getId()) {
-                        number = number.substring(i + 1, number.length());
-                    }
-                }
-
-                if (area == null) continue;
-                else phone = new Phone(Long.parseLong(number), area);
-
-            } else {
-
-                phone = new Phone(Long.parseLong(number), country);
-            }
-
-            // see if number is already blocked
-            if (UserPhoneDAO.isAlreadyBlocked(new UserPhone(LUserDAO.getThisUser(), phone, false))) {
+            // check if number is already blocked
+            if (LUserPhoneDAO.isBlocked(new UserPhone(LUserDAO.getThisUser(), phone, false))) {
                 continue;
             }
 
-            final Call call = new Call(name, phone);
-
-            if (!numberList.contains(number)) {
-                callList.add(call);
-                numberList.add(number);
-            }
+            callList.add(new Call(name, phone));
         }
 
         return callList;
--- app/src/main/java/com/enoughspam/step/database/domain/Phone.java
+++ app/src/main/java/com/enoughspam/step/database/domain/Phone.java
@@ -4,6 +4,8 @@ import android.support.annotation.NonNull;
 
 import com.enoughspam.step.annotation.NonNegative;
 import com.enoughspam.step.database.domain.abstracts.Domain;
+import com.enoughspam.step.database.wideDao.AreaDAO;
+import com.enoughspam.step.database.wideDao.CountryDAO;
 
 /**
  * Created by Hugo Castelani
@@ -64,5 +66,42 @@ public class Phone extends Domain {
 
     public void setArea(Area area) {
         this.area = area;
+    }
+
+    /**
+     * @return: returns a phone if it exists or null if it doesn't exist
+     */
+    public static Phone generateObject(@NonNull String number, @NonNull final String iso) {
+        // removing any 0 left
+        number = String.valueOf(Long.parseLong(number));
+
+        final Country country = CountryDAO.findByIso(iso);
+        Phone phone = null;
+
+        // some countries don't have area code
+        if (!country.getMask().isEmpty()) {
+
+            // discover number's area and country
+            StringBuilder areaCode = new StringBuilder(50);
+            Area area = null;
+            final int codeLength = String.valueOf(country.getCode()).length();
+
+            for (int i = 0; i < codeLength; i++) {
+                areaCode.append(number.charAt(i));
+
+                area = AreaDAO.findByCode(Integer.valueOf(areaCode.toString()));
+                if (area != null && area.getState().getCountry().getId() == country.getId()) {
+                    number = number.substring(i + 1, number.length());
+                }
+            }
+
+            if (area != null) phone = new Phone(Long.parseLong(number), area);
+
+        } else {
+
+            phone = new Phone(Long.parseLong(number), country);
+        }
+
+        return phone;
     }
 }
