dd1b62d9d68cfbc3476fa3ea98f10687d0bb5d8e
Hugo <HugoCastelaniBP@gmail.com>
2017-08-10 17:52:00 -0300

    Call blocker functional

---
+++ app/src/main/java/com/android/internal/telephony/ITelephony.java
@@ -0,0 +1,12 @@
+package com.android.internal.telephony;
+
+/**
+ * Created by Hugo Castelani
+ * Date: 10/08/17
+ * Time: 15:00
+ */
+
+public interface ITelephony {
+    boolean endCall();
+    void silenceRinger();
+}
---
+++ app/src/main/java/com/enoughspam/step/call/PhoneCallReceiver.java
@@ -0,0 +1,105 @@
+package com.enoughspam.step.call;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Build;
+import android.telephony.TelephonyManager;
+
+import com.android.internal.telephony.ITelephony;
+import com.enoughspam.step.database.domain.Phone;
+import com.enoughspam.step.database.domain.UserPhone;
+import com.enoughspam.step.database.localDao.LUserDAO;
+import com.enoughspam.step.database.localDao.LUserPhoneDAO;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by Hugo Castelani
+ * Date: 08/08/17
+ * Time: 16:41
+ */
+
+public class PhoneCallReceiver extends BroadcastReceiver {
+    private Context mContext;
+    private TelephonyManager mTelephonyManager;
+    private AudioManager mAudioManager;
+    private Phone mPhone;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals("android.intent.action.PHONE_STATE")) {
+            mContext = context;
+            mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+
+            // mute until discover whether is number blocked
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                final NotificationManager notificationManager =
+                        (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N &&
+                        !notificationManager.isNotificationPolicyAccessGranted()) {
+
+                    final Intent intentB = new Intent(android.provider.Settings
+                                    .ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                    mContext.getApplicationContext().startActivity(intentB);
+
+                } else {
+
+                    mAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                }
+            } else {
+                //noinspection deprecation
+                mAudioManager.setStreamMute(AudioManager.STREAM_RING, true);
+            }
+
+            mTelephonyManager = (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+            final String number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            final String iso = mTelephonyManager.getNetworkCountryIso().toUpperCase();
+
+            mPhone = Phone.generateObject(number, iso);
+
+            // check if it's blocked
+            if (LUserPhoneDAO.isBlocked(new UserPhone(LUserDAO.getThisUser(), mPhone, false))) {
+                disconnectPhoneITelephony();
+
+            } else {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    mAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                } else {
+                    //noinspection deprecation
+                    mAudioManager.setStreamMute(AudioManager.STREAM_RING, false);
+                }
+            }
+        }
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private void disconnectPhoneITelephony() {
+        ITelephony telephonyService;
+        TelephonyManager telephony = (TelephonyManager)
+                mContext.getSystemService(Context.TELEPHONY_SERVICE);
+
+        try {
+            Class aClass = Class.forName(telephony.getClass().getName());
+            Method method = aClass.getDeclaredMethod("getITelephony");
+            method.setAccessible(true);
+            telephonyService = (ITelephony) method.invoke(telephony);
+            telephonyService.endCall();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
