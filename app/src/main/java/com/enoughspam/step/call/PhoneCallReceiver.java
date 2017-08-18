378390fd25602c404305c9d38d9510fdd11ab87b
Hugo <HugoCastelaniBP@gmail.com>
2017-08-18 15:58:58 -0300

    Start SMS block implementation

--- app/src/main/java/com/enoughspam/step/call/PhoneCallReceiver.java
+++ app/src/main/java/com/enoughspam/step/call/PhoneCallReceiver.java
@@ -4,6 +4,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
 import android.telephony.TelephonyManager;
 
 import com.android.internal.telephony.ITelephony;
@@ -24,6 +28,7 @@ import java.lang.reflect.Method;
 
 public class PhoneCallReceiver extends BroadcastReceiver {
     private Context mContext;
+    private Intent mIntent;
     private TelephonyManager mTelephonyManager;
     private AudioManager mAudioManager;
 
@@ -31,31 +36,73 @@ public class PhoneCallReceiver extends BroadcastReceiver {
     @SuppressWarnings("deprecation")
     public void onReceive(Context context, Intent intent) {
         mContext = context;
+        mIntent = intent;
         mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
 
         // mute until discover whether is number blocked
         mAudioManager.setStreamMute(AudioManager.STREAM_RING, true);
 
-        if (intent.getAction().equals("android.intent.action.PHONE_STATE")) {
-            mTelephonyManager = (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
-            final String number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-            final String iso = mTelephonyManager.getNetworkCountryIso().toUpperCase();
-
-            // NullPointerException is going to be thrown if it's an outcoming call
-            final boolean isBlocked;
-            try {
-                DAOHandler.init(mContext);
-                final Phone phone = Phone.generateObject(number, iso);
-                isBlocked = LUserPhoneDAO.isBlocked(new UserPhone(LUserDAO.getThisUser(), phone, false));
-            } catch (NullPointerException e) {
-                return;
-            }
-
-            // check if it's blocked
-            if (isBlocked) disconnectPhoneITelephony();
+        switch (intent.getAction()) {
+            case "android.intent.action.PHONE_STATE":
+                handlePhoneCall();
+                break;
+            case "android.provider.Telephony.SMS_RECEIVED":
+                handleSms();
+                break;
+            default: break;
         }
 
         mAudioManager.setStreamMute(AudioManager.STREAM_RING, false);
+    }
+
+    private void handlePhoneCall() {
+        mTelephonyManager = (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+        final String number = mIntent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+        final String iso = mTelephonyManager.getNetworkCountryIso().toUpperCase();
+
+        // NullPointerException is going to be thrown if it's an outcoming call
+        final boolean isBlocked;
+        try {
+            DAOHandler.init(mContext);
+            final Phone phone = Phone.generateObject(number, iso);
+            isBlocked = LUserPhoneDAO.isBlocked(new UserPhone(LUserDAO.getThisUser(), phone, false));
+        } catch (NullPointerException e) {
+            return;
+        }
+
+        // check if it's blocked
+        if (isBlocked) disconnectPhoneITelephony();
+    }
+
+    @SuppressWarnings("deprecation")
+    private void handleSms() {
+        final Bundle bundle = mIntent.getExtras();
+
+        try {
+            if (bundle != null) {
+                final Object[] pdusObject = (Object[]) bundle.get("pdus");
+
+                for (int i = 0; i < pdusObject.length; i++) {
+
+                    SmsMessage smsMessage;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        SmsMessage[] msgs = Telephony.Sms.Intents.getMessagesFromIntent(mIntent);
+                        smsMessage = msgs[0];
+
+                    } else {
+
+                        Object pdus[] = (Object[]) bundle.get("pdus");
+                        smsMessage = SmsMessage.createFromPdu((byte[]) pdus[0]);
+                    }
+
+                    String phoneNumber = smsMessage.getDisplayOriginatingAddress();
+
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @SuppressWarnings({"rawtypes", "unchecked"})
