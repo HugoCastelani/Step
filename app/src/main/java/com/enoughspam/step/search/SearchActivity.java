cc9a7a90616e6adf36fa313a56e249fc272c7277
Hugo <HugoCastelaniBP@gmail.com>
2017-11-11 11:37:53 -0200

    Finish SearchActivity and SearchAdapter

--- app/src/main/java/com/enoughspam/step/search/SearchActivity.java
+++ app/src/main/java/com/enoughspam/step/search/SearchActivity.java
@@ -2,6 +2,8 @@ package com.enoughspam.step.search;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.StringRes;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
@@ -9,14 +11,15 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.enoughspam.step.R;
 import com.enoughspam.step.abstracts.AbstractActivity;
-import com.enoughspam.step.util.decorator.ListDecorator;
 
 public class SearchActivity extends AbstractActivity {
     EditText mEditText;
+    ImageView mClearIcon;
     LinearLayout mProgressBar;
     LinearLayout mNoResults;
     RecyclerView mRecyclerView;
@@ -39,6 +42,8 @@ public class SearchActivity extends AbstractActivity {
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
 
+        mClearIcon = (ImageView) findViewById(R.id.search_clear_icon);
+        mProgressBar = (LinearLayout) findViewById(R.id.search_progress_bar);
         mProgressBar = (LinearLayout) findViewById(R.id.search_progress_bar);
         mNoResults = (LinearLayout) findViewById(R.id.search_no_results);
         mRecyclerView = (RecyclerView) findViewById(R.id.search_recycler_view);
@@ -46,33 +51,73 @@ public class SearchActivity extends AbstractActivity {
 
     @Override
     protected void initActions() {
+        mClearIcon.setOnClickListener(view -> mEditText.getText().clear());
+
+        final SearchAdapter adapter = new SearchAdapter(this);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(getBaseContext(),
+                LinearLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setAdapter(adapter);
+        mRecyclerView.setLayoutManager(layoutManager);
+
         mEditText.addTextChangedListener(new TextWatcher() {
             @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
             @Override public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
             @Override
             public void afterTextChanged(Editable editable) {
-                final String query = editable.toString();
-
-                if (!query.isEmpty()) {
-                    mRecyclerView.setAdapter(new SearchAdapter(query));
-                    mRecyclerView.setVisibility(View.VISIBLE);
-                    mNoResults.setVisibility(View.GONE);
-                }
+                adapter.setQuery(editable.toString());
             }
         });
-
-        final LinearLayoutManager layoutManager = new LinearLayoutManager(getBaseContext(),
-                LinearLayoutManager.VERTICAL, false);
-
-        mRecyclerView.setLayoutManager(layoutManager);
-
-        ListDecorator.init(getBaseContext());
-        ListDecorator.addAdaptableMargins(mRecyclerView);
     }
 
     @Override
-    protected void initFragment() {
+    protected void initFragment() {}
 
+    public void showProgressBar() {
+        if (mRecyclerView.getVisibility() != View.GONE) {
+            mRecyclerView.setVisibility(View.GONE);
+        }
+
+        if (mNoResults.getVisibility() != View.GONE) {
+            mNoResults.setVisibility(View.GONE);
+        }
+
+        if (mProgressBar.getVisibility() != View.VISIBLE) {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void showNoResults() {
+        if (mRecyclerView.getVisibility() != View.GONE) {
+            mRecyclerView.setVisibility(View.GONE);
+        }
+
+        if (mProgressBar.getVisibility() != View.GONE) {
+            mProgressBar.setVisibility(View.GONE);
+        }
+
+        if (mNoResults.getVisibility() != View.VISIBLE) {
+            mNoResults.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void showRecyclerView() {
+        if (mNoResults.getVisibility() != View.GONE) {
+            mNoResults.setVisibility(View.GONE);
+        }
+
+        if (mProgressBar.getVisibility() != View.GONE) {
+            mProgressBar.setVisibility(View.GONE);
+        }
+
+        if (mRecyclerView.getVisibility() != View.VISIBLE) {
+            mRecyclerView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void showSnackbar(@StringRes final int message) {
+        Snackbar.make(findViewById(android.R.id.content), getResources().getString(message),
+                Snackbar.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/enoughspam/step/search/SearchAdapter.java
+++ app/src/main/java/com/enoughspam/step/search/SearchAdapter.java
@@ -1,16 +1,22 @@
 package com.enoughspam.step.search;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.enoughspam.step.R;
 import com.enoughspam.step.database.dao.wide.UserDAO;
 import com.enoughspam.step.database.domain.User;
+import com.enoughspam.step.profile.ProfileActivity;
+import com.enoughspam.step.util.AnimUtils;
 import com.enoughspam.step.util.Listeners;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -24,25 +30,80 @@ import de.hdodenhof.circleimageview.CircleImageView;
  */
 
 public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.MyViewHolder> {
+    final SearchActivity mActivity;
+
     final ArrayList<User> userList;
 
-    public SearchAdapter(@NonNull final String query) {
+    Listeners.ListListener<User> mListListener;
+    Listeners.AnswerListener mAnswerListener;
+
+    public SearchAdapter(@NonNull final SearchActivity activity) {
+        mActivity = activity;
         userList = new ArrayList<>();
+    }
 
-        UserDAO.get().findByKey("-Kwk6q46QWGzxvgi_OW4", new Listeners.UserListener() {
-            @Override
-            public void onUserRetrieved(@NonNull User retrievedUser) {
-                userList.add(retrievedUser);
-                notifyDataSetChanged();
-            }
+    public SearchAdapter setQuery(@NonNull final String query) {
+        userList.clear();
 
-            @Override public void onError() {}
-        });
+        if (query.isEmpty()) {
+            mActivity.showNoResults();
 
-        /*UserDAO.get().findByKey(2, retrievedUser -> {
-            userList.add(retrievedUser);
-            notifyDataSetChanged();
-        });*/
+        } else {
+
+            mActivity.showProgressBar();
+            UserDAO.get().filterByUsername(query, getListListener(), getAnswerListener());
+        }
+
+        return this;
+    }
+
+    private Listeners.ListListener<User> getListListener() {
+        if (mListListener == null) {
+            mListListener = new Listeners.ListListener<User>() {
+                @Override
+                public void onItemAdded(@NonNull User user) {
+                    for (final User loopUser : userList) {
+                        if (loopUser.getKey().equals(user.getKey())) {
+                            return;
+                        }
+                    }
+
+                    userList.add(user);
+                    notifyDataSetChanged();
+                    mActivity.showRecyclerView();
+                }
+
+                @Override
+                public void onItemRemoved(@NonNull User user) {
+                    for (final User loopUser : userList) {
+                        if (loopUser.getKey().equals(user.getKey())) {
+                            userList.remove(loopUser);
+                        }
+                    }
+                }
+            };
+        }
+
+        return mListListener;
+    }
+
+    private Listeners.AnswerListener getAnswerListener() {
+        if (mAnswerListener == null) {
+            mAnswerListener = new Listeners.AnswerListener() {
+                @Override public void onAnswerRetrieved() {
+                    if (userList.isEmpty()) {
+                        mActivity.showNoResults();
+                    }
+                }
+
+                @Override
+                public void onError() {
+                    mActivity.showSnackbar(R.string.something_went_wrong);
+                }
+            };
+        }
+
+        return mAnswerListener;
     }
 
     @Override
@@ -55,8 +116,24 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.MyViewHold
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         final User user = userList.get(position);
-        Picasso.with(holder.username.getContext()).load(user.getPicURL()).into(holder.userPic);
-        holder.username.setText(user.getUsername());
+
+        Picasso.with(holder.mUsername.getContext()).load(user.getPicURL())
+                .into(holder.mUserPic, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        AnimUtils.fadeOutFadeIn(holder.mProgressBar, holder.mUserPic);
+                    }
+
+                    @Override public void onError() {}
+                });
+
+        holder.mUsername.setText("@" + user.getUsername());
+
+        holder.mCardView.setOnClickListener(view -> {
+            final Intent intent = new Intent(mActivity, ProfileActivity.class);
+            intent.putExtra("user", user);
+            mActivity.startActivity(intent);
+        });
     }
 
     @Override
@@ -65,13 +142,17 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.MyViewHold
     }
 
     protected static class MyViewHolder extends RecyclerView.ViewHolder {
-        final CircleImageView userPic;
-        final TextView username;
+        final CardView mCardView;
+        final ProgressBar mProgressBar;
+        final CircleImageView mUserPic;
+        final TextView mUsername;
 
         public MyViewHolder(View itemView) {
             super(itemView);
-            userPic = (CircleImageView) itemView.findViewById(R.id.search_item_profile_user_pic);
-            username = (TextView) itemView.findViewById(R.id.search_item_profile_username);
+            mCardView = (CardView) itemView.findViewById(R.id.search_item_profile_card);
+            mProgressBar = (ProgressBar) itemView.findViewById(R.id.search_item_profile_progress_bar);
+            mUserPic = (CircleImageView) itemView.findViewById(R.id.search_item_profile_user_pic);
+            mUsername = (TextView) itemView.findViewById(R.id.search_item_profile_username);
         }
     }
 }
