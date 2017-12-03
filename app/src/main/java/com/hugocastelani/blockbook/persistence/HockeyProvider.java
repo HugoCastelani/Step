df91908978daf4735731ab62b42b70572b177ffd
Hugo <HugoCastelaniBP@gmail.com>
2017-12-03 11:10:08 -0200

    Update project

---
+++ app/src/main/java/com/hugocastelani/blockbook/persistence/HockeyProvider.java
@@ -0,0 +1,18 @@
+package com.hugocastelani.blockbook.persistence;
+
+/**
+ * Created by Hugo Castelani
+ * Date: 02/12/17
+ * Time: 16:21
+ * Class's name is a pun: Hawk key provider ;D
+ */
+
+public final class HockeyProvider {
+    public static final String USER_KEY = "java.lang.String##QBVKSk|xwjbA=!oIQX!r@pw_^K$=1Z";
+    public static final String IS_DARK = "java.lang.String##3nF$|$i$z!UeRfM8!FfTrxTUoU3Y+F";
+    public static final String NETWORKS = "java.lang.String##=E8mSZQ*FQXssM!ZJ^!#FVUmwrYN4S";
+    public static final String SERVICES = "java.lang.String##Dzfy3$KDLGr%w&_GECW*sR#^A2f%hE";
+    public static final String IS_INTRO_ALL_SET = "java.lang.String##U1igiPn1j|rc$1?8^z?T*tO=OYsMBO";
+    public static final String DENUNCIATION_AMOUNT = "java.lang.String##FmxfNRcSzgaieR?r#vgl*6!#0+aC=c";
+    public static final String DESCRIPTION = "description_";
+}
